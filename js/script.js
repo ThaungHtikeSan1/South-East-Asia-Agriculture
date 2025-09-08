d891044ccea33946ac16db19707e16fb3c73736b
ThaungHtikeSan1 <khantnyaroo12@gmail.com>
2025-09-08 15:55:14 +0630

    nav add more

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ const menu = document.getElementById("menu");
       menu.classList.toggle("hidden");
     });
 
+    // Journals
+function checkAuth(event) {
+    event.preventDefault(); 
+    const isSignedIn = false; 
+
+    if (isSignedIn) {
+      
+      window.location.href = "/journals.html";
+    } else {
+      
+      alert("⚠️ You must sign in to continue!");
+      window.location.href = "./register.html";
+    }
+  }
+
+
